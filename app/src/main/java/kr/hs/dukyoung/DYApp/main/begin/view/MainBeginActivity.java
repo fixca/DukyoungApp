4e125c1945c8a515d5a72532610f535cf1a6f70d
AI3992 <lizamong0225@gmail.com>
2021-11-04 16:28:36 +0900

    2021/11/04/16:28

--- app/src/main/java/kr/hs/dukyoung/DYApp/main/begin/view/MainBeginActivity.java
+++ app/src/main/java/kr/hs/dukyoung/DYApp/main/begin/view/MainBeginActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import kr.hs.dukyoung.DYApp.MainActivity;
 import kr.hs.dukyoung.DYApp.jaehoon.R;
+import kr.hs.dukyoung.DYApp.request.IDoInBackground;
+import kr.hs.dukyoung.DYApp.request.Request;
+import kr.hs.dukyoung.DYApp.request.URLRequest;
+import kr.hs.dukyoung.DYApp.schedule.adapter.CalendarAdapter;
+import kr.hs.dukyoung.DYApp.timetable.view.TimeTableActivity;
 
 public class MainBeginActivity extends AppCompatActivity {
 
@@ -17,9 +24,9 @@ public class MainBeginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_begin);
 
-        Button cafeteria_button = findViewById(R.id.cafeteria_button);
-        Button timetable_button = findViewById(R.id.timetable_button);
-        Button Notice_button = findViewById(R.id.Notice_button);
+        ImageButton cafeteria_button = findViewById(R.id.cafeteria_button);
+        ImageButton timetable_button = findViewById(R.id.timetable_button);
+        ImageButton schedule_button = findViewById(R.id.schedule_button);
 
         Intent intent = getIntent();
 
@@ -32,7 +39,31 @@ public class MainBeginActivity extends AppCompatActivity {
         //int grade = intent.getIntExtra("gratetext",0);
 
         TextView menuView = (TextView) findViewById(R.id.menuText);
-        menuView.setText(grade+"학년"+stuclass+"반 학생");
+        menuView.setText(grade+"학년 "+stuclass+"반 학생");
+
+        Request request = new Request(new IDoInBackground() {
+            @Override
+            public void doInBackground() {
+                try {
+                    URLRequest urlRequest = new URLRequest("https://dukyoung.hs.kr/board.list?mcode=1210");
+                    String html = urlRequest.sendRequest();
+                    Log.i("html",html);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        schedule_button.setOnClickListener(view ->{
+            Intent intent12 = new Intent(MainBeginActivity.this, CalendarAdapter.DayViewHolde.class);
+            startActivity(intent12);
+        });
+
+
+
+        request.execute();
 
         //여기 아래 부분은 엑티비티 넘길 예정
 
